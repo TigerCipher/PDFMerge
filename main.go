8d8794bade1fceb07ca4e6bb10380457c89f3304
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2023-12-14 12:34:26 -0600

    window close event

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"gioui.org/app"
+	"gioui.org/io/system"
+	"gioui.org/unit"
+	"os"
 )
 
 func main() {
 	go func() {
-		w := app.NewWindow()
-
+		w := app.NewWindow(app.Title("PDF Merge"), app.Size(unit.Dp(800), unit.Dp(600)))
 		for {
-			w.NextEvent()
+			e := w.NextEvent()
+			switch e.(type) {
+			case system.DestroyEvent:
+				fmt.Println("Closing window")
+				os.Exit(0)
+			}
 		}
 	}()
 	app.Main()
