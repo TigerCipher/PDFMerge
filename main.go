87750f598ce1720fe5bfaee7af321dfdf7734825
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2023-12-14 12:43:44 -0600

    Added button for testing

--- main.go
+++ main.go
@@ -4,17 +4,32 @@ import (
 	"fmt"
 	"gioui.org/app"
 	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
 	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
 	"os"
 )
 
 func main() {
 	go func() {
 		w := app.NewWindow(app.Title("PDF Merge"), app.Size(unit.Dp(800), unit.Dp(600)))
+		var ops op.Ops
+		var startButton widget.Clickable
+		th := material.NewTheme()
+
 		for {
 			e := w.NextEvent()
-			switch e.(type) {
-			case system.DestroyEvent:
+
+			if e, ok := e.(system.FrameEvent); ok {
+				gtx := layout.NewContext(&ops, e)
+				btn := material.Button(th, &startButton, "Start")
+				btn.Layout(gtx)
+				e.Frame(gtx.Ops)
+			}
+
+			if _, ok := e.(system.DestroyEvent); ok {
 				fmt.Println("Closing window")
 				os.Exit(0)
 			}
